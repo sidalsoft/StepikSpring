ab358b5368cd10861a67a935920d9fe2a173b8ea
Saidali Saburov <ssaburov@transparent.com>
2022-01-24 12:20:11 +0500

    first commit

---
+++ src/main/java/com/stepik/demo/StepikSpringApplication.java
@@ -0,0 +1,13 @@
+package com.stepik.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepikSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepikSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepik/demo/StepikSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepik.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepikSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
